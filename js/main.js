9d1cbedaa499d7b57a3179638043535ef52b2750
zakaria-magdy2 <zekomagdy613@gmail.com>
2025-06-29 04:41:59 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const passwordInput = document.getElementById("password");
+const togglePassword = document.getElementById("togglePassword");
+
+togglePassword.addEventListener("click", () => {
+  const isPassword = passwordInput.type === "password";
+  passwordInput.type = isPassword ? "text" : "password";
+
+  togglePassword.src = isPassword
+    ? "../images/hide-pass.png"
+    : "../images/show-pass.png";
+});
