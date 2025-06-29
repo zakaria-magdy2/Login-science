78de8284c399c4d7bc128a82a1915eefe261cc61
zakaria-magdy2 <zekomagdy613@gmail.com>
2025-06-29 04:53:25 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
-const passwordInput = document.getElementById("password");
-const togglePassword = document.getElementById("togglePassword");
+document.addEventListener("DOMContentLoaded", function () {
+  const passwordInput = document.getElementById("password");
+  const togglePassword = document.getElementById("togglePassword");
 
-togglePassword.addEventListener("click", () => {
-  const isPassword = passwordInput.type === "password";
-  passwordInput.type = isPassword ? "text" : "password";
+  togglePassword.addEventListener("click", () => {
+    const isPassword = passwordInput.type === "password";
+    passwordInput.type = isPassword ? "text" : "password";
 
-  togglePassword.src = isPassword ? "../images/hide-pass.png" : "../images/show-pass.png";
+    togglePassword.src = isPassword
+      ? "../images/hide-pass.png"
+      : "../images/show-pass.png";
+  });
 });
